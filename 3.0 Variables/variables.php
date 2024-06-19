fe439b96146d57b1636911878a2fe2c50bdc9a01
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-19 14:25:28 +0800

    update

--- 3.0 Variables/variables.php
+++ 3.0 Variables/variables.php
@@ -7,16 +7,23 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>Declaring PHP Variables</title>
 </head>
+
+<h3>This is Global Scope</h3>
+<p><strong>Variable with global scope</strong></p>
 <?php
-//Variable with global scope
-//A variable declared outside a function has a GLOBAL SCOPE and can only be accessed outside a function:
 $x = 5;
 function myTest() {
     //using x inside this function will generate an error
-    echo "<p>Variable x inside function is: $x</p>";
+    //echo "<p>Variable x inside function is: $x</p>";
+    //A variable declared outside a function has a GLOBAL SCOPE 
+    //and can only be accessed outside a function:
 }
 myTest();
 echo "<p>Variable x inside the function is: $x</p>";
+?>
+
+<h3>This is Local Scope</h3>
+<?php
 ?>
 </body>
 </html>
