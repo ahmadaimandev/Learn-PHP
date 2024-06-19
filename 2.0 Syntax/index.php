c2e45cdf4a9efea66848bb523df9a3556ac00a6c
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-19 10:09:17 +0800

    update

---
+++ 2.0 Syntax/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Syntax</title>
+</head>
+<body>
+    <?php
+    //php code goes inside here
+    echo "Hello World";
+    echo "Hello World";
+    echo "Hello World";
+    ?>
+</body>
+</html>
