3bab93c75890131dc45e5c134b4ab1f5c388250c
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-19 09:51:36 +0800

    learn php

---
+++ 1.0 Home/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyFirst PHP Program</title>
+</head>
+<body>
+    <?php
+    echo "My first PHP Script!";
+    ?>
+</body>
+</html>
