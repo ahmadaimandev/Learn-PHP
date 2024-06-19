ece869537a6c5263bdd7c355b61ba59484e6d522
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-19 16:00:24 +0800

    update

---
+++ 4.0 Print and Echo/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <title>Echo & Print</title>
+</head>
+<style>
+    
+    * {
+        font-family: "Open Sans", sans-serif;
+    }
+
+    header {
+        width: 50%;
+        margin: 0 auto;
+    }
+
+    h2 {
+        text-align: center;
+        text-transform: uppercase;
+    }
+</style>
+<body>
+    <header>
+        <h2>Echo and Print Statement</h2>
+    </header>
+
+    <main>
+        <h3>The PHP echo statement</h3>
+        <code>
+            <?php
+
+            ?>
+        </code>
+    </main>
+</body>
+</html>
