638c6255fb4e9f9461deeb67399372c8080c74d4
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-20 09:55:24 +0800

    update

--- 6.0 Strings/6.2 Modify Strings/index.php
+++ 6.0 Strings/6.2 Modify Strings/index.php
@@ -3,13 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modify Strings<title>
+    <title>Modify Strings</title>
 </head>
 <body>
-    <h2>Modify Strings</h2>
+    <h2>Modifying Strings</h2>
     <?php
-    $uppertext = "Ahmad Aiman";
-    echo "This is uppercase text: " .$uppertext;
+    $justname = "Aiman";
+    echo strtoupper($justname) ."<br>"; //uppercase functions
+    echo strtolower($justname) ."<br>"; //lowercase functions
+    echo str_replace("Aiman", "Ali", $justname); //function to replace other strings
+    
     ?>
 </body>
 </html>
