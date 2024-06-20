00bb2d07a985430cc9d4d8aa440a9c53a7529017
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-20 14:57:16 +0800

    update

--- 6.0 Strings/6.4 Slicing/index.php
+++ 6.0 Strings/6.4 Slicing/index.php
@@ -8,16 +8,24 @@
 <body>
     <h2 style="text-transform: uppercase; text-align:center; font-size: 40px;">Slicing Strings</h2>
 
+
+    <h3 style="text-transform: uppercase;">Slicing Strings</h3>
     <?php
     //Slicing strings
     $string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Our big pizza
     $slice = substr($string,2,10); // We start at position 2 (C) and take 10 slices
-    echo $slice; //output "CDEFGHIJKL"
+    echo "The output is: $slice"; //output "CDEFGHIJKL"
+    echo "<br>";
     ?>
 
+    <hr>
+    
+    <h3 style="text-transform: uppercase;">Slicing to the end</h3>
     <?php
     //Slicing to the end
-    
+    $EndString = "ABCDEFGHIJKLMN";
+    $startSlice = substr($EndString,6); //Start the slice at index 6 and go all the way to the end:
+    echo $startSlice; // The output "GHIJKLMN"
     ?>
 </body>
 </html>
