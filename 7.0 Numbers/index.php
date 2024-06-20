4b2b13c51b80984c275cbff53ff40ad565e39922
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-20 16:09:14 +0800

    update

---
+++ 7.0 Numbers/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Numbers</title>
+</head>
+<style>
+    h2 {
+        border: 1px solid;
+        width: 20%;
+        margin: 20px auto;
+        text-align: center;
+        text-transform: uppercase;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        transition: 0.2s ease-in-out;
+        border-radius: 20px;
+    }
+
+    h2:hover {
+        background-color: red;
+        border: 1px solid red;
+        color: white;
+        transition: 0.2s ease-in-out;
+    }
+</style>
+<body>
+    <h2>Numbers In PHP</h2>
+
+    <main class="first-main">
+        
+    </main>
+</body>
+</html>
