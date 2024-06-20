2c6fb8c3ee82f7cf7e751b64004e43153364e8cb
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-20 17:46:33 +0800

    update

---
+++ 17. Array/17.2 Indexed Array/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indexed Array</title>
+</head>
+    <style>
+        h3 {
+            text-align: center;
+            text-transform: uppercase;
+            border: 1px solid black;
+            border-radius: 10px;
+            background-color: black;
+            color: white;
+            width: 20%;
+            margin: 20px auto;
+            transition: 0.3s ease-in-out;
+        }
+        h3:hover {
+            background-color: white;
+            border: 1px solid black;
+            color: black;
+            border-radius: 0px;
+        }
+    </style>
+<body>
+    <h3>Indexed Array</h3>
+</body>
+</html>
