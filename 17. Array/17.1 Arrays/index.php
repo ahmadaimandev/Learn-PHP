136b9e53b9fd4aa5a93a30c0777a7af90dfcd830
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-20 16:38:33 +0800

    update

---
+++ 17. Array/17.1 Arrays/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array In PHP</title>
+</head>
+<body>
+    <?php
+    $carList = array("Proton, Perodua", "Honda", "Toyota");
+
+    print ($carList);
+    ?>
+</body>
+</html>
