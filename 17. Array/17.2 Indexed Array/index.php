f30b01698268fb81ad2684f33d08dd9de24288be
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-20 17:56:13 +0800

    update

--- 17. Array/17.2 Indexed Array/index.php
+++ 17. Array/17.2 Indexed Array/index.php
@@ -16,15 +16,32 @@
             width: 20%;
             margin: 20px auto;
             transition: 0.3s ease-in-out;
+            font-size: 18px;
         }
         h3:hover {
             background-color: white;
             border: 1px solid black;
             color: black;
             border-radius: 0px;
+            cursor: pointer;
         }
     </style>
 <body>
     <h3>Indexed Array</h3>
+    <p>Indexed array start at 0</p>
+    <?php
+    $CarList = array("Proton", "Perodua");
+    var_dump( $CarList );
+    echo "<p>It will start count from 0 and so on</p>"
+    ?>
+
+    <h3>Accessed Indexed Array</h3>
+    <?php
+    $AccessCarNum = array("Proton", "Honda", "Perodua", "Toyota");
+    echo "This car name is: $AccessCarNum[0] <br>";
+    echo "This car name is: $AccessCarNum[1] <br>";
+    echo "This car name is: $AccessCarNum[2] <br>";
+    echo "This car name is: $AccessCarNum[3]";
+    ?>
 </body>
 </html>
