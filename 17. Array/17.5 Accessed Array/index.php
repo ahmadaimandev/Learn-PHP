5617878c6deec623d12b1d88bda6ad2f40a5c3a9
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-21 11:52:17 +0800

    update

---
+++ 17. Array/17.5 Accessed Array/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accessed Array</title>
+</head>
+<style>
+    h2 {
+        margin:  20px auto;
+        text-align: center;
+        text-transform: uppercase;
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
+<body>
+    <h2>How to access Array</h2>
+    <?php
+    $cars = array("Proton","Perodua", "Ferrari");
+    echo "Output: ". $cars[2] ."<br>"; //Note: The first item has index 0.
+
+    //To access items from an associative array, use the key name:
+    $CarList = array("Brand" => "Proton", "Model" => "Bezza", "Engine Capacity" => 1.3);
+    echo "Output: ". $CarList['Brand'];
+    ?>
+</body>
+</html>
