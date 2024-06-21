780ee06ff1b3340296c1e093a5f44ed81468992a
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-21 10:02:24 +0800

    update

--- 17. Array/17.4 Create Array/index.php
+++ 17. Array/17.4 Create Array/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Create Arrays</title>
 </head>
 <style>
     h3 {
@@ -35,5 +35,7 @@
     ];
     var_dump( $CarList );
     ?>
+
+    <h3>Array Keys  </h3>
 </body>
 </html>
