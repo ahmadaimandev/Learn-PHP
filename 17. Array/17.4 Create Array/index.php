f401d301b9eaab26e8ccaf20cab69eaddfd2ec3a
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-21 09:12:55 +0800

    update

---
+++ 17. Array/17.4 Create Array/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    h3 {
+        text-align: center;
+        margin: 20px auto;
+        text-transform: capitalize;
+    }
+</style>
+<body>
+    <h3>Create an array</h3>
+    <?php
+    
+    ?>
+</body>
+</html>
