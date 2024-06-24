38ddd9b3d08926f32ba8d1597eb2a39329126d36
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-24 08:23:07 +0800

    update

---
+++ 17. Array/17.7 Add Array/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Array</title>
+</head>
+<style>
+    h2 {
+        text-align: center;
+        margin: 20px auto;
+        text-transform: capitalize;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+    <h2>Add Array</h2>
+    <?php
+    //use bracket to add new array into existing array
+    ?>
+</body>
+</html>
