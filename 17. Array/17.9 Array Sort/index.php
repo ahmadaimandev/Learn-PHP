7d4e360f1d1fa4d8bb4989dec51f23553230a8c0
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-24 11:44:45 +0800

    update

--- 17. Array/17.9 Array Sort/index.php
+++ 17. Array/17.9 Array Sort/index.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sorting In Array</title>
 </head>
+<style>
+    h2 {
+        text-align: center;
+        text-transform: capitalize;
+        margin: 20px auto;
+    }
+</style>
 <body>
     
 </body>
