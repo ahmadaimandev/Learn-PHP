c1dfbc0f33f12198dca9921de0793f04e9b97b64
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-24 14:53:51 +0800

    update

--- 17. Array/17.9 Array Sort/index.php
+++ 17. Array/17.9 Array Sort/index.php
@@ -13,5 +13,13 @@
     }
 </style>
     <h2>sort array ascending order - sort()</h2>
+    <?php
+    
+    ?>
+    <h2>sort array descending order - rsort()</h2>
+    <h2>sort array (ascending order), according to value - asort()</h2>
+    <h2>sort array (ascending order), according to key - ksort()</h2>
+    <h2>sort array (descending order), according to value - arsort()</h2>
+    <h2>sort array (descending order), according to key - krsort()</h2>
 <body/>
 </html>
