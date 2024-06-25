2567958b35735997d4c1852fb9a54cfef9f31a00
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-25 15:26:36 +0800

    update

---
+++ 2. Forms/1.0 Forms Handling/$_get/get.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>POST Method</h2>
+    <div class="post-form">
+        <form action="getdata.php" method="POST">
+            <label for="Name">Username
+                <input type="text" name="Uname" required>
+            </label>
+            <label for="Email">Email Address
+                <input type="email" name="Uemail" required>
+            </label>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</body>
+</html>
