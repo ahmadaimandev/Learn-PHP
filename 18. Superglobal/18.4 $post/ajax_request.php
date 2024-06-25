842c32f22c122b4a559aee553f48564eec4ae9d1
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-25 12:34:04 +0800

    update

--- 18. Superglobal/18.4 $post/ajax_request.php
+++ 18. Superglobal/18.4 $post/ajax_request.php
@@ -8,12 +8,27 @@
 <style>
     h2 {
         text-align: center;
-        margin: 20px auto;
-        text-transform: uppercase;
         font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-size: 40px;
+        margin: 20px auto;
     }
+    button {
+    width: 400px;
+    height: 50px;
+    border-radius: 5px;
+    border: 1px solid black;
+    background-color: white;
+    display: block; /* Make the button a block element */
+    margin: 50px auto; /* Center the button horizontally */
+    text-align: center; /* Center the text inside the button */
+    }
+
 </style>
 <body>
     <h2>Using Javascript HTTP REQUEST</h2>
+    <button onclick="myFunction()">Click Me</button>
 </body>
+<script>
+    
+</script>
 </html>
