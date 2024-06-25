9bd74f74665cd39666a8adb92b9a2d51f107faf1
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-25 12:10:05 +0800

    update

---
+++ 18. Superglobal/18.4 $post/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$POST METHOD</title>
+</head>
+<style>
+    h2 {
+        text-align: center;
+        margin: 20px auto;
+        text-transform: uppercase;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+</style>
+<body>
+    <h2>$_POST in HTML Forms</h2>
+    <form action="" method="post"></form>
+</body>
+</html>
