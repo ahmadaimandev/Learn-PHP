9fa4f6e1883d270a914257ded223d493742e43ca
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-25 09:47:03 +0800

    update

--- 17. Array/18.0 Multidimensional Array/index.php
+++ 17. Array/18.0 Multidimensional Array/index.php
@@ -5,18 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Multidimensional Array</title>
 </head>
+<style>
+    h2 {
+        margin: 20px auto;
+        text-align: center;
+        text-transform: uppercase;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+</style>
 <body>
+    <h2>Multidimensional Array</h2>
     <?php
     $cars = array(
         array("Volvo", 22, 18),
         array("BMW", 15, 13),
         array("Saab", 5,2),
         array("Land Rover", 17, 15)
+
+    /*
+    Now the two-dimensional $cars array contains four arrays, and it has two indices: row and column.
+    To get access to the elements of the $cars array we must point to the two indices (row and column):
+    */
+
     );
     echo $cars[0][0] . ": In stock: " . $cars[0][1] . ", sold: " . $cars[0][2] . ".<br>";
     echo $cars[1][0] . ": In stock: " . $cars[1][1] . ", sold: " . $cars[1][2] . ".<br>";
     echo $cars[2][0] . ": In stock: " . $cars[2][1] . ", sold: " . $cars[2][2] . ".<br>";
     echo $cars[3][0] . ": In stock: " . $cars[3][1] . ", sold: " . $cars[3][2] . ".<br>";
+
+    //using for loop
+    for ($row = 0; $row < 4; $row++) {
+        echo "<p><b>Row number $row</b></p>";
+        echo "<ul>";
+        for ($col = 0; $col < 3; $col++) {
+            echo "<li>" . $cars[$row][$col] . "</li>";
+        }
+        echo "</ul>";
+    }
     ?>
 </body>
 </html>
