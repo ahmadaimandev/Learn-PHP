48e370557d7a510751caf6ad2557cb7aa30fb507
Ahmad Aiman <aimanzulbahari@gmail.com>
2024-06-25 14:36:39 +0800

    update

---
+++ 1. Introduction/18. Superglobal/18.5 $get/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$_GET request</title>
+</head>
+<body>
+    <form action="getData.php" method="GET">
+        <label for="uName">Username
+            <input type="text" name="userName">
+        </label>
+        <label for="uEmail">Email
+            <input type="email" name="userEmail">
+        </label>
+        <input type="submit" value="Get data PHP">
+    </form>
+</body>
+</html>
