5069d130de5bbba9fa3bbf70e67a3d9e3bdbf42b
Aiman <aimanzulbahari@gmail.com>
2024-06-27 11:57:08 +0800

    update

--- 2. Forms/2.0 Forms Validation/form.php
+++ 2. Forms/2.0 Forms Validation/form.php
@@ -9,11 +9,26 @@
 
 </style>
 <body>
-    <form action="post">
-        <label for="useName">Name:
-            <input type="text" name="username" required>
-        </label>
-        <label for="userEmail"></label>
-    </form>
+    <div class="form-background">
+        <h3>Form Validation</h3>
+        <form method="post" action="">
+            <label for="name">Name:
+                <input type="text" name="Username">
+            </label>
+            <label for="email">Email:
+                <input type="email" name="UserEmail">
+            </label>
+            <label for="website">Website:
+                <input type="text" name="UserWebsite">
+            </label>
+            <label for="comment">Leave your comment here:
+                <textarea name="UserComment" rows="5" cols="40"></textarea>
+            </label>
+            <label for="gender">Please select your gender:</label>
+            <input type="radio" name="UserGender"> Male
+            <input type="radio" name="UserGender"> Female
+            <input type="radio" name="UserGender"> Non-Binary
+        </form>
+    </div>
 </body>
 </html>
