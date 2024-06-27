1bdca05a1e662cc8aa7333a7717036c6e31210e7
Aiman <aimanzulbahari@gmail.com>
2024-06-27 12:03:36 +0800

    update

--- 2. Forms/2.0 Forms Validation/form.php
+++ 2. Forms/2.0 Forms Validation/form.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,22 +13,36 @@
     <div class="form-background">
         <h3>Form Validation</h3>
         <form method="post" action="">
-            <label for="name">Name:
-                <input type="text" name="Username">
+            <label for="username">Name:
+                <input type="text" id="username" name="Username">
             </label>
+            <br>
             <label for="email">Email:
-                <input type="email" name="UserEmail">
+                <input type="email" id="email" name="UserEmail">
             </label>
+            <br>
             <label for="website">Website:
-                <input type="text" name="UserWebsite">
+                <input type="text" id="website" name="UserWebsite">
             </label>
+            <br>
             <label for="comment">Leave your comment here:
-                <textarea name="UserComment" rows="5" cols="40"></textarea>
+                <textarea id="comment" name="UserComment" rows="5" cols="40"></textarea>
             </label>
-            <label for="gender">Please select your gender:</label>
-            <input type="radio" name="UserGender"> Male
-            <input type="radio" name="UserGender"> Female
-            <input type="radio" name="UserGender"> Non-Binary
+            <br>
+            <fieldset>
+                <legend>Please select your gender:</legend>
+                <label for="male">
+                    <input type="radio" id="male" name="UserGender" value="male"> Male
+                </label>
+                <label for="female">
+                    <input type="radio" id="female" name="UserGender" value="female"> Female
+                </label>
+                <label for="nonBinary">
+                    <input type="radio" id="nonBinary" name="UserGender" value="non-binary"> Non-Binary
+                </label>
+            </fieldset>
+            <br>
+            <input type="submit" value="Submit Form">
         </form>
     </div>
 </body>
