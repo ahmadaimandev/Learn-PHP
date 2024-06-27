90ea026ebb859bd0d2d72c55f98173cde8002c41
Aiman <aimanzulbahari@gmail.com>
2024-06-27 15:34:02 +0800

    update

--- 2. Forms/3.0 Forms Required/index.php
+++ 2. Forms/3.0 Forms Required/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,56 +13,51 @@
     html {
         font-family: "Poppins", sans-serif;
     }
+    .error {
+        color: #FF0000;
+    }
 </style>
+
 <body>
     <?php
-    //define variables and set to empty values
-    $UsernameErr = $UserEmailErr = $GenderErr = $WebsiteErr = "";
-    $Username = $UserEmail = $UserComment = $UserGender = $UserWebsite = "";
+    // define variables and set to empty values
+    $nameErr = $emailErr = $genderErr = $websiteErr = "";
+    $name = $email = $gender = $comment = $website = "";
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        #Username
-        if(empty($_POST[$Username])) {
-            $UsernameErr = "Username is required in this field";
-        }
-        else {
-            $Username = test_input($_POST["UserName"]);
-        }
-
-        #User Email
-        if(empty($_POST[$UserEmail])) {
-            $UserEmailErr = "Username is required in this field";
-        }
-        else {
-            $UserEmail = test_input($_POST["UserEmail"]);
-        }
-
-        #User Comment
-        if (empty($_POST["UserComment"])) {
-            $UserComment = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
         } else {
-            $UserComment = test_input($_POST["UserComment"]);
-        }        
+            $name = test_input($_POST["name"]);
+        }
 
-        #User Gender
-        if(empty($_POST[$UserGender])) {
-            $GenderErr = "Username is required in this field";
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
         }
-        else {
-            $UserGender = test_input($_POST["UserGender"]);
+
+        if (empty($_POST["website"])) {
+            $website = "";
+        } else {
+            $website = test_input($_POST["website"]);
         }
-        
-        #User Website
-        if(empty($_POST[$UserWebsite])) {
-            $UserWebsiteErr = "Username is required in this field";
+
+        if (empty($_POST["comment"])) {
+            $comment = "";
+        } else {
+            $comment = test_input($_POST["comment"]);
         }
-        else {
-            $UserWebsite = test_input($_POST["UserWebsite"]);
+
+        if (empty($_POST["gender"])) {
+            $genderErr = "Gender is required";
+        } else {
+            $gender = test_input($_POST["gender"]);
         }
     }
 
-    function test_input($data) {
+    function test_input($data)
+    {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
@@ -69,24 +65,41 @@
     }
     ?>
 
-    <div class="form-background">
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <div class="user-info">
-                <label for="UserName"><b>Username</b>
-                    <input type="text" name="Username">
-                    <span class="error">* <?php echo $UsernameErr;?></span>
-                </label>
-                <label for="email">Email:
-                    <input type="email" name="UserEmail">
-                    <span class="error">* <?php echo $UserEmailErr;?></span>
-                </label>
-            </div>
-            <div class="user-comment"></div>
-            <div class="user-gender"></div>
-            <div class="button-submit">
-                <input type="Submit" value="Submit Form Application">
-            </div>
-        </form>
-    </div>
+    <h2>PHP Form Validation Example</h2>
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        Name: <input type="text" name="name">
+        <span class="error">* <?php echo $nameErr; ?></span>
+        <br><br>
+        E-mail: <input type="text" name="email">
+        <span class="error">* <?php echo $emailErr; ?></span>
+        <br><br>
+        Website: <input type="text" name="website">
+        <span class="error"><?php echo $websiteErr; ?></span>
+        <br><br>
+        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+        <br><br>
+        Gender:
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="other">Other
+        <span class="error">* <?php echo $genderErr; ?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
+    ?>
 </body>
+
 </html>
