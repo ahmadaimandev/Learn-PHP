48435e002d774c632591b10f66720abb435dbaea
Aiman <aimanzulbahari@gmail.com>
2024-06-28 08:55:31 +0800

    update the code

--- 2. Forms/3.0 Forms Required/index.php
+++ 2. Forms/3.0 Forms Required/index.php
@@ -9,97 +9,23 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Forms Required</title>
 </head>
-<style>
-    html {
-        font-family: "Poppins", sans-serif;
-    }
-    .error {
-        color: #FF0000;
-    }
-</style>
-
 <body>
     <?php
-    // define variables and set to empty values
+    #define variables and set to empty values
+
+    //This will hold error message for the required fields
     $nameErr = $emailErr = $genderErr = $websiteErr = "";
     $name = $email = $gender = $comment = $website = "";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["name"])) {
-            $nameErr = "Name is required";
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST["name"])) {
+            $nameErr = "Username required";
         } else {
             $name = test_input($_POST["name"]);
         }
 
-        if (empty($_POST["email"])) {
-            $emailErr = "Email is required";
-        } else {
-            $email = test_input($_POST["email"]);
-        }
-
-        if (empty($_POST["website"])) {
-            $website = "";
-        } else {
-            $website = test_input($_POST["website"]);
-        }
-
-        if (empty($_POST["comment"])) {
-            $comment = "";
-        } else {
-            $comment = test_input($_POST["comment"]);
-        }
-
-        if (empty($_POST["gender"])) {
-            $genderErr = "Gender is required";
-        } else {
-            $gender = test_input($_POST["gender"]);
-        }
+        
     }
-
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-    ?>
-
-    <h2>PHP Form Validation Example</h2>
-    <p><span class="error">* required field</span></p>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Name: <input type="text" name="name">
-        <span class="error">* <?php echo $nameErr; ?></span>
-        <br><br>
-        E-mail: <input type="text" name="email">
-        <span class="error">* <?php echo $emailErr; ?></span>
-        <br><br>
-        Website: <input type="text" name="website">
-        <span class="error"><?php echo $websiteErr; ?></span>
-        <br><br>
-        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-        <br><br>
-        Gender:
-        <input type="radio" name="gender" value="female">Female
-        <input type="radio" name="gender" value="male">Male
-        <input type="radio" name="gender" value="other">Other
-        <span class="error">* <?php echo $genderErr; ?></span>
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-
-    <?php
-    echo "<h2>Your Input:</h2>";
-    echo $name;
-    echo "<br>";
-    echo $email;
-    echo "<br>";
-    echo $website;
-    echo "<br>";
-    echo $comment;
-    echo "<br>";
-    echo $gender;
     ?>
 </body>
-
 </html>
