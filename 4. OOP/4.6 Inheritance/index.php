3c43e35e0a6fa72efef9d7212e311dbccae7715b
Aiman <aimanzulbahari@gmail.com>
2024-07-15 10:34:26 +0800

    update

--- 4. OOP/4.6 Inheritance/index.php
+++ 4. OOP/4.6 Inheritance/index.php
@@ -1,52 +1,74 @@
 <?php
-class Fruits {
+class Fruits
+{
     public $name;
     public $color;
-    public function __construct($name, $color) {
+    public function __construct($name, $color)
+    {
         $this->name = $name;
         $this->color = $color;
     }
-    public function intro() {
+    public function intro()
+    {
         echo "The fruit is {$this->name} and the color is: {$this->color}";
     }
 }
 
-class strawberry extends Fruits { //strawberry is inherited from fruits
+class strawberry extends Fruits
+{ //strawberry is inherited from fruits
     //this mean strawberry class can use the public $name and $color properties as well as the public__construct() and intro() methods from
     //fruit class
-    public function message() {
+    public function message()
+    {
         echo "Am i a fruit or a berry?";
     }
 }
 
 $strawberry = new strawberry("Strawberry", "Red");
 $strawberry->message();
-$strawberry->intro() ;
+$strawberry->intro();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h2>PHP - Inheritance and the Protected Access Modifier</h2>
     <?php
-    class Car {
-        public $CarName;
-        public $CarColor;
-        public function __construct($CarName, $CarColor) {
-            $this->CarName = $CarName;
-            $this->CarColor = $CarColor;
+    class Car
+    {
+        public $Carname;
+        public $Carcolor;
+        public function __construct($Carname, $Carcolor)
+        {
+            $this->Carname = $Carname;
+            $this->Carcolor = $Carcolor;
         }
-        protected function intro() {
-            echo "This car name is {$this->CarName} and the color is{$this->CarColor}";
+        protected function intro()
+        {
+            echo "The fruit is {$this->Carname} and the color is {$this->Carcolor}.";
         }
-
-            
     }
+
+    class Mustang extends Car
+    {
+        public function message()
+        {
+            echo "Am I a Muscle Car? ";
+        }
+    }
+
+    // Try to call all three methods from outside class
+    $mustang = new mustang("mustang", "grey");  // OK. __construct() is public
+    $mustang->message(); // OK. message() is public
+    $mustang->intro(); // ERROR. intro() is protected
     ?>
 </body>
+
 </html>
