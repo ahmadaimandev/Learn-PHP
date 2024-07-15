258bb8b6c1edeee02cf64a10063d587147251ff4
Aiman <aimanzulbahari@gmail.com>
2024-07-15 11:56:52 +0800

    update code

--- 3. Advance/3.1 Date and Time/index.php
+++ 3. Advance/3.1 Date and Time/index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h2>
-        Get A Date
-    </h2>
+    <h2>Get A Date</h2>
+    <p>Date Format</p>
+    <?php
+    echo "Today is " . date("Y/m/d") . "<br>";
+    echo "Today is " . date("Y.m.d") . "<br>";
+    echo "Today is " . date("Y-m-d") . "<br>";
+    echo "Today is " . date("l");
+    ?>
 </body>
+
 </html>
