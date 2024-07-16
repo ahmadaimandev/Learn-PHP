a9def1aacd1c883253609dc9ba013b8b47708271
Aiman <aimanzulbahari@gmail.com>
2024-07-16 10:14:50 +0800

    update

---
+++ 3. Advance/3.2 Include/Eg.2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to PHP Page</h1>
+    <?php
+    include 'vars.php';
+    echo "I have a $carColor $carBrand";
+    ?>
+</body>
+</html>
---
+++ 3. Advance/3.2 Include/Eg.2/vars.php
@@ -0,0 +1,4 @@
+<?php
+    $carColor = "red";
+    $carBrand = "Ferrari";
+?>
