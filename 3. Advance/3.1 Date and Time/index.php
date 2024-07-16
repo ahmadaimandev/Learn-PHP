25f08b9005273fc94e5fc73a82632df749f3054b
Aiman <aimanzulbahari@gmail.com>
2024-07-16 08:29:51 +0800

    update

--- 3. Advance/3.1 Date and Time/index.php
+++ 3. Advance/3.1 Date and Time/index.php
@@ -71,7 +71,9 @@
     <h2>More Date examples: #2</h2>
     <?php
     //The example below outputs the number of days until 4th of July:
-    
+    $d1 = strtotime("July 04");
+    $d2 = ceil(($d1 - time()) / 60 / 60 / 24);
+    echo "There are " . $d2 . " days until 4th of July.";
     ?>
 </body>
 
