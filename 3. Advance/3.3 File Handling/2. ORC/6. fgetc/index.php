3b8f92032260e2aa9a7aabbfc9552c8bdfc35df7
Aiman <aimanzulbahari@gmail.com>
2024-07-17 09:27:55 +0800

    update

--- 3. Advance/3.3 File Handling/2. ORC/6. fgetc/index.php
+++ 3. Advance/3.3 File Handling/2. ORC/6. fgetc/index.php
@@ -6,6 +6,13 @@
     <title>fgetc</title>
 </head>
 <body>
-    
+    <h2>fgetc</h2>
+    <?php
+    $myfile = fopen("ref_role.json","r") or die("Unable to open  the file");
+    while(!feof($myfile)) {
+        echo fgets($myfile);
+    }
+    fclose($myfile);
+    ?>
 </body>
 </html>
