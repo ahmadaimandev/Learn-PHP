86702d6ca500dd4030086bf9c8798bdaa8e4a796
Aiman <aimanzulbahari@gmail.com>
2024-07-18 08:37:37 +0800

    update

--- 3. Advance/3.3 File Handling/3. CW/fwrite/index.php
+++ 3. Advance/3.3 File Handling/3. CW/fwrite/index.php
@@ -8,7 +8,7 @@
 <body>
     <h2>Writing File</h2>
     <?php
-    $myFile = fopen("Note.txt", "w") or die("Unable to open the file");
+    $myFile = fopen("starwars.txt", "w") or die("Unable to open the file");
     $txt = "This is John Doe. Luke I'm Your Father\n";
     fwrite( $myFile, $txt );
     $txt = "NOOOOOOOOO!!!!\n";
