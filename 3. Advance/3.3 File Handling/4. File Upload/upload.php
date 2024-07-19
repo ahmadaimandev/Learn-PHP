917ca09c950cfec0714bb808d39c68b699854e94
Aiman <aimanzulbahari@gmail.com>
2024-07-19 11:21:24 +0800

    finished code

--- 3. Advance/3.3 File Handling/4. File Upload/upload.php
+++ 3. Advance/3.3 File Handling/4. File Upload/upload.php
@@ -41,7 +41,11 @@ if($uploadOk == 0) {
 
     //if everything is ok, try to upload file
 } else {
-    if(move_uploaded_file)
+    if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). "Has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
 }
 
 ?>
