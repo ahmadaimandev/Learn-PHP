463e46af019bd620a642a02cf5dc9a021787a3c1
Aiman <aimanzulbahari@gmail.com>
2024-07-19 16:23:18 +0800

    update code

--- 3. Advance/5. cookie/index.php
+++ 3. Advance/5. cookie/index.php
@@ -3,7 +3,7 @@
     $cookie_value = "John Doe";
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day  1 day=24×60×60=86400 seconds
     ?>
-
+<!-- Note: The setcookie() function must appear BEFORE the <html> tag. -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
