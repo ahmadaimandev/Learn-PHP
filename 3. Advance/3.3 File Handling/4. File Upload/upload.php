7d50db12418778cb67e4c194725a738126fc45f1
Aiman <aimanzulbahari@gmail.com>
2024-07-19 11:00:04 +0800

    update

---
+++ 3. Advance/3.3 File Handling/4. File Upload/upload.php
@@ -0,0 +1,9 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $targer_dir .basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+
+//This will check image an actual image or fake image
+?>
