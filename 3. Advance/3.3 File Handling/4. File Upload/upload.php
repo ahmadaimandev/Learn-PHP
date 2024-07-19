dc1797d481e7dadacff92b0b9de8d664e971c5ca
Aiman <aimanzulbahari@gmail.com>
2024-07-19 11:07:56 +0800

    update

--- 3. Advance/3.3 File Handling/4. File Upload/upload.php
+++ 3. Advance/3.3 File Handling/4. File Upload/upload.php
@@ -6,4 +6,28 @@ $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 
 //This will check image an actual image or fake image
+if(isset($POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image- " .$check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image";
+        $uploadOk = 0;
+    }
+}
+
+//Check if file already exists
+if(file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+//Check file size 
+if($_FILES["fileToUpload"]["size"] > 5000000) {
+    echo "sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+//Allo certain file formats
 ?>
