60c9c64339f4cfa8c4eed83b0b648841d69d5501
Aiman <aimanzulbahari@gmail.com>
2024-07-19 16:22:02 +0800

    update

--- 3. Advance/5. cookie/index.php
+++ 3. Advance/5. cookie/index.php
@@ -3,6 +3,7 @@
     $cookie_value = "John Doe";
     setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day  1 day=24×60×60=86400 seconds
     ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +13,13 @@
 </head>
 <body>
     <h2>PHP Create/Retrieve a Cookie</h2>
-    
+    <?php
+    if (!isset($_COOKIE[$cookie_name])) {
+        echo "Cookie named '" . $cookie_name . "' is not set!";
+    } else {
+        echo "Cookie '" . $cookie_name . "' is set!<br>";
+        echo "Value is: " . $_COOKIE[$cookie_name];
+    }
+    ?>
 </body>
 </html>
