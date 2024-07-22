73801de3f7c02dacde35661f490cf2b171d2718f
Aiman <aimanzulbahari@gmail.com>
2024-07-22 10:01:20 +0800

    continue later

--- 3. Advance/3.7 Filters/3.7.1 Filters/index.php
+++ 3. Advance/3.7 Filters/3.7.1 Filters/index.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filter</title>
 </head>
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+
+    th,
+    td {
+        padding: 5px;
+    }
+</style>
+
 <body>
     <table>
         <tr>
@@ -13,9 +28,10 @@
         </tr>
         <?php
         foreach (filter_list() as $id => $filter) {
-            echo '<tr><td>' .$filter .'<td></td>' .filter_id($filter) .'</td></tr>';
+            echo '<tr><td>' . $filter . '</td><td>' . filter_id($filter) . '</td></tr>';
         }
         ?>
     </table>
 </body>
+
 </html>
