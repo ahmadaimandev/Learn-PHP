e7d6b981e251c31ccb1469c8039faa3c87f61903
Aiman <aimanzulbahari@gmail.com>
2024-07-23 08:34:53 +0800

    update code

--- 3. Advance/3.7 Filters/3.7.1 Filters/index.php
+++ 3. Advance/3.7 Filters/3.7.1 Filters/index.php
@@ -57,19 +57,15 @@
         echo ("Integer is not valid") . "<br>";
     }
     ?>
-
-    <?php
-    $SecondInt = "100";
-    if (!filter_var($SecondInt, FILTER_VALIDATE_INT) === true) {
-        echo ("Integer is valid");
-    } else {
-        echo ("Integer is not valid") . "<br>";
-    }
-    ?>
     
     <h2>filter_var() And With Problem 0 </h2>
     <?php
-    
+    $int = 0;
+    if (filter_var($int, FILTER_VALIDATE_INT) === 0 || !filter_var($int, FILTER_VALIDATE_INT) === false) {
+        echo ("Integer is valid");
+    }else {
+        echo ("Integer is not valid") . "<br>";
+    }
     ?>
 </body>
 
