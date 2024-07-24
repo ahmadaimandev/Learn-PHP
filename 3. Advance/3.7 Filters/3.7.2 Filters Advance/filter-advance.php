70e9384501bb45fd469bbd0fce8997a49f602949
Aiman <aimanzulbahari@gmail.com>
2024-07-24 08:09:23 +0800

    update

--- 3. Advance/3.7 Filters/3.7.2 Filters Advance/filter-advance.php
+++ 3. Advance/3.7 Filters/3.7.2 Filters Advance/filter-advance.php
@@ -8,7 +8,17 @@
 <body>
     <h1>Filter Advance</h1>
     <?php
-    
+    echo "<h2>Validate and integer within a range</h2>";
+    #This example uses the filter_var() function to check if a variable is both of type INT, and between 1 and 200:
+        $int = 100;
+        $min = 1;
+        $max = 200;
+
+        if (filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range"=> $min, "max_range"=>$max))) === false) {
+            echo ("Variable value is not within the legal range");
+        }else {
+            echo ("Variable value is within the legal range");
+        }
     ?>
 </body>
 </html>
