722a3adc1aa96b305386069ab3d657004ce42ed2
Aiman <aimanzulbahari@gmail.com>
2024-07-24 09:21:17 +0800

    update code

--- 3. Advance/3.8 Callback Functions/index.php
+++ 3. Advance/3.8 Callback Functions/index.php
@@ -6,6 +6,21 @@
     <title>Callback Functions</title>
 </head>
 <body>
-    
+    <h1>First Example</h1>
+    <?php
+    #Pass a callback to PHP's array_map() function to calculate the length of every string in an array:
+    function my_callback($item) {
+        return strlen($item); # strlen = returns the length of a string in bytes
+    }
+
+    $strings = [
+        "Hello",
+        "World",
+        "PHP",
+        "is awesome!",
+    ];
+    $lengths = array_map("my_callback", $strings);
+    print_r($lengths);
+    ?>
 </body>
 </html>
