a8fe370f7799866c49a23c60f5ba9d7751175ec2
Aiman <aimanzulbahari@gmail.com>
2024-07-24 08:16:13 +0800

    update

--- 3. Advance/3.7 Filters/3.7.2 Filters Advance/filter-advance.php
+++ 3. Advance/3.7 Filters/3.7.2 Filters Advance/filter-advance.php
@@ -20,5 +20,29 @@
             echo ("Variable value is within the legal range");
         }
     ?>
+
+    <?php
+    echo "<h2>Validate IPv6 Address</h2>";
+    #This example uses the filter_var() function to check if the variable $ip is a valid IPv6 address:
+        $ipOwnership = array(
+            "2001:db8::/32",
+            "2001:20::/28",
+            "fc00::/7",
+            "100::/8",
+            "200::/7",
+            "400::/6",
+            "800::/5",
+            "1000::/4",
+            "2001:0db8:85a3:08d3:1319:8a2e:0370:7334"
+        );
+
+        foreach($ipOwnership as $ipList) {
+            if (!filter_var($ipList, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
+                echo ("IPv6 address is valid") ."<br>";
+            }else {
+                echo ("IPv6 address is not valid") ."<br>";
+            }
+        }
+    ?>
 </body>
 </html>
