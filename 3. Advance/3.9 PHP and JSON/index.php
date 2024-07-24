38dcf1451e8fc4ea4a5ea44caba466a885fc1132
Aiman <aimanzulbahari@gmail.com>
2024-07-24 15:32:48 +0800

    update

--- 3. Advance/3.9 PHP and JSON/index.php
+++ 3. Advance/3.9 PHP and JSON/index.php
@@ -6,7 +6,7 @@
     <title>PHP and JSON</title>
 </head>
 <body>
-    <h1>Example 1</h1>
+    <h1>json_encode() - Example 1</h1>
     <?php
     echo "<h2>PHP - JSON_encode()</h2>";
     echo "<p>The json_encode() function is used to encode a value to JSON format.</p>";
@@ -19,6 +19,17 @@
         "Jim" => 27
     );
     echo json_encode($age) ."<br>";
+    ?>
+    <h1>json_encode() - Example 2</h1>
+    <?php
+    #This example shows how to encode an indexed array into a JSON array:
+    $cars = array (
+        "Volvo",
+        "BMW",
+        "Mercedes",
+        "Audi"
+    );
+    echo json_encode($cars) ."<br>";
     ?>
 </body>
 </html>
