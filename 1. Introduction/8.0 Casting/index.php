7b5b25c69a3f04d871005660ca9f4c39810144ec
Aiman <aimanzulbahari@gmail.com>
2024-07-25 14:18:52 +0800

    update

--- 1. Introduction/8.0 Casting/index.php
+++ 1. Introduction/8.0 Casting/index.php
@@ -1,16 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Casting</title>
 </head>
+
 <body>
     <h1>Cast to String</h1>
     <p>To cast to string, use the (string) statement:</p>
     <h2>Example 1</h2>
     <?php
+    $a = 5;       // Integer
+    $b = 5.34;    // Float
+    $c = "hello"; // String
+    $d = true;    // Boolean
+    $e = NULL;    // NULL
     
+    $a = (string) $a;
+    $b = (string) $b;
+    $c = (string) $c;
+    $d = (string) $d;
+    $e = (string) $e;
+
+    //To verify the type of any object in PHP, use the var_dump() function:
+    var_dump($a);
+    var_dump($b);
+    var_dump($c);
+    var_dump($d);
+    var_dump($e);
     ?>
 </body>
+
 </html>
