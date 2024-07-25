5f7454d81b6cf83d280fdfec01482eb6f25f4945
Aiman <aimanzulbahari@gmail.com>
2024-07-25 15:20:05 +0800

    update code

--- 1. Introduction/8.0 Casting/index.php
+++ 1. Introduction/8.0 Casting/index.php
@@ -91,6 +91,42 @@
     var_dump($f);
     var_dump($g);
     ?>
+
+    <h1>Cast To Boolean</h1>
+    <p>To cast to boolean, use the (bool) statement:</p>
+    <h2>Example 4</h2>
+    <?php
+    $a = 5;       // Integer
+    $b = 5.34;    // Float
+    $c = 0;       // Integer
+    $d = -1;      // Integer
+    $e = 0.1;     // Float
+    $f = "hello"; // String
+    $g = "";      // String
+    $h = true;    // Boolean
+    $i = NULL;    // NULL
+    
+    $a = (bool) $a;
+    $b = (bool) $b;
+    $c = (bool) $c;
+    $d = (bool) $d;
+    $e = (bool) $e;
+    $f = (bool) $f;
+    $g = (bool) $g;
+    $h = (bool) $h;
+    $i = (bool) $i;
+    
+    //To verify the type of any object in PHP, use the var_dump() function:
+    var_dump($a);
+    var_dump($b);
+    var_dump($c);
+    var_dump($d);
+    var_dump($e);
+    var_dump($f);
+    var_dump($g);
+    var_dump($h);
+    var_dump($i);
+    ?>
 </body>
 
 </html>
