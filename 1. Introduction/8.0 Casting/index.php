0c4055fedef4622e41276f9bf537620d64b1e05b
Aiman <aimanzulbahari@gmail.com>
2024-07-25 16:17:49 +0800

    update code

--- 1. Introduction/8.0 Casting/index.php
+++ 1. Introduction/8.0 Casting/index.php
@@ -178,6 +178,30 @@
     $myCar = (array) $myCar;
     var_dump($myCar); ?>
     ?>
+
+    <h1>Cast To Objects</h1>
+    <p>To cast to object, use the (object) statement:</p>
+    <h2>Example 6</h2>
+    <?php
+    $a = 5;       // Integer
+    $b = 5.34;    // Float
+    $c = "hello"; // String
+    $d = true;    // Boolean
+    $e = NULL;    // NULL
+    
+    $a = (object) $a;
+    $b = (object) $b;
+    $c = (object) $c;
+    $d = (object) $d;
+    $e = (object) $e;
+    
+    //To verify the type of any object in PHP, use the var_dump() function:
+    var_dump($a);
+    var_dump($b);
+    var_dump($c);
+    var_dump($d);
+    var_dump($e);
+    ?>
 </body>
 
 </html>
