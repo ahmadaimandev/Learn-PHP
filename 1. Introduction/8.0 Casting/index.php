2bbf77a45c42ffc16fbc9eca79c5c135023d33df
Aiman <aimanzulbahari@gmail.com>
2024-07-25 14:10:54 +0800

    update

---
+++ 1. Introduction/8.0 Casting/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Casting</title>
+</head>
+<body>
+    <h1>Cast to String</h1>
+    <p>To cast to string, use the (string) statement:</p>
+    <h2>Example 1</h2>
+    <?php
+    
+    ?>
+</body>
+</html>
