8b1ffeca57d9655ce26b33ee4d69b29ba2e8e53d
Aiman <aimanzulbahari@gmail.com>
2024-07-29 08:09:19 +0800

    update

--- 3. Advance/4.0 PHP Exceptions/index.php
+++ 3. Advance/4.0 PHP Exceptions/index.php
@@ -30,5 +30,19 @@
      * In the example above, the type of exception is Exception and the variable name is $e.
      */
     ?>
+
+    <?php
+    echo "<h1>The try...catch...finally Statement</h1>";
+    echo "<h2>Example 2</h2>";
+    echo "<p>Show a message when an exception is thrown and then indicate that the process has ended:</p>";
+
+    function divideoperation($dividen, $divisor) {
+        if($divisor == 0) {
+            throw new Exception("Division by Zero");
+        }
+        return $dividen / $divisor;
+    }
+
+    ?>
 </body>
 </html>
