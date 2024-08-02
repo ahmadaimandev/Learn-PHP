412e9dbd0a891d75c4936cb3eabdd3c951d2c06b
Aiman <aimanzulbahari@gmail.com>
2024-08-02 09:04:38 +0800

    Finish PHP math function subtopic

--- 1. Introduction/9.0 Math/index.php
+++ 1. Introduction/9.0 Math/index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Math</title>
 </head>
+
 <body>
     <h1 style="text-align: center;">PHP - PI() function</h1>
     <?php
-    echo "Output: " .(pi()) ."<br>";
+    echo "Output: " . (pi()) . "<br>";
     ?>
 
     <h1 style="text-align: center;">PHP - min() and max() functions</h1>
@@ -27,5 +29,21 @@
     echo "Square root value: 100" . "<br>";
     echo "Output Square Root Value: " . (sqrt(100)) . "<br>";
     ?>
+
+    <h1 style="text-align: center;">PHP - round() function</h1>
+    <?php
+    $roundValue = 67.89;
+    echo "Value: " . ($roundValue) . "<br>";
+    echo "Output Round Value: " . (round($roundValue)) . "<br>";
+    ?>
+
+    <h1 style="text-align: center;">PHP - random() function</h1>
+    <?php
+    #Generate random number without control the lowest and highest number
+    echo "Output Random Value: " . (rand()) . "<br>";
+    #Generate random number with control the lowest and highest number
+    echo "Output Random Value: " . (rand(10, 200)) . "<br>";
+    ?>
 </body>
+
 </html>
