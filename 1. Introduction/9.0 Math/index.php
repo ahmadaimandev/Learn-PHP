8435f6c49f759d7a28956419be3af42a2859a12d
Aiman <aimanzulbahari@gmail.com>
2024-08-02 08:25:19 +0800

    update

--- 1. Introduction/9.0 Math/index.php
+++ 1. Introduction/9.0 Math/index.php
@@ -6,9 +6,15 @@
     <title>PHP - Math</title>
 </head>
 <body>
-    <h1 style="text-align: center;">PHP - Math</h1>
+    <h1 style="text-align: center;">PHP - PI() function</h1>
     <?php
     echo "Output: " .(pi()) ."<br>";
+    ?>
+
+    <h1 style="text-align: center;">PHP - min() and max() functions</h1>
+    <?php
+    echo "Output Min Value: " . (min(0, 150, 30, 20, -8, -200)) . "<br>";
+    echo "Output Max Value: " . (max(0, 150, 30, 20, -8, -200)) . "<br>";
     ?>
 </body>
 </html>
