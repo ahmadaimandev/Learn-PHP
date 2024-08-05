ad3c49d3a42859f30a81602ce330b723ac173e72
Aiman <aimanzulbahari@gmail.com>
2024-08-05 08:17:49 +0800

    update

--- 1. Introduction/11.0 Magic Constant/index.php
+++ 1. Introduction/11.0 Magic Constant/index.php
@@ -1,11 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Magic Constant</title>
 </head>
+<style>
+
+</style>
+
 <body>
-    
+    <h1>Magic Constant</h1>
+    <div class="magic-constant">
+        <h2 class="title">__CLASS__</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
+    <div class="magic-constant">
+        <h2 class="title">__DIR__</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
+    <div class="magic-constant">
+        <h2 class="title">__FILE__</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
+    <div class="magic-constant">
+        <h2 class="title">__FUNCTION__</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
+    <div class="magic-constant">
+        <h2 class="title">__LINE__</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
+    <div class="magic-constant">
+        <h2 class="title">__METHOD__</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
+    <div class="magic-constant">
+        <h2 class="title">__NAMESPACE__</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
+    <div class="magic-constant">
+        <h2 class="title">__TRAIT__</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
+    <div class="magic-constant">
+        <h2 class="title">ClassName::class</h2>
+        <code>
+            <?php
+            
+            ?>
+        </code>
+    </div>
 </body>
+
 </html>
